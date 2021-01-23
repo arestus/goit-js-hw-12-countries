d5a004e17b3b085eb84f75cc05a3335734ea2e13
arestus <arestus@gmail.com>
2021-01-23 12:38:11 +0200

    Complete

--- src/js/renderCountry.js
+++ src/js/renderCountry.js
@@ -3,16 +3,16 @@ import country from '../templates/country.hbs';
 import countries from '../templates/countries.hbs';
 import getRefs from './refs';
 const ref = getRefs();
-function renderCountry(arrCountry) {
-  if (arrCountry.length === 1) {
-    const markup = country(arrCountry);
+function renderCountry(countriesList) {
+  if (countriesList.length === 1) {
+    const markup = country(countriesList);
 
     ref.Render.innerHTML = markup;
-  } else if (arrCountry.length > 1 && arrCountry.length < 10) {
-    const markup = countries(arrCountry);
+  } else if (countriesList.length > 1 && countriesList.length < 10) {
+    const markup = countries(countriesList);
 
     ref.Render.innerHTML = markup;
-  } else if (arrCountry.length > 9) {
+  } else if (countriesList.length > 9) {
     notifications.onTooMuch();
   } else {
     notifications.onNull;
